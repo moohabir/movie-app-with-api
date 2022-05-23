f1dbfc10926f7916790bec9c8f9a21c0dca4c599
moohabir <87937848+moohabir@users.noreply.github.com>
2022-05-23 18:49:02 +0200

    .l,

--- src/App.js
+++ src/App.js
@@ -6,34 +6,17 @@ import "./styles.css";
 
 export default function App() {
   const [searchValue, setSearchValue] = useState("");
-  const [movies, setMovies] = useState([
-    {
-      Title: "Avengers: Infinity War",
-      Year: "2018",
-      imdbID: "tt4154756",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-    },
-    {
-      Title: "Captain America: Civil War",
-      Year: "2016",
-      imdbID: "tt3498820",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
 
-  async function Getapi(searchValue) {
-    const url = `"http://www.omdbapi.com/?s=${searchValue}&apikey=c3eb04e2"`;
+  const Getapi = async () => {
+    const url = "http://www.omdbapi.com/?s=war&apikey=c3eb04e2";
     const response = await fetch(url);
     const responseJson = await response.json();
     setMovies(responseJson.Search);
-  }
+  };
   useEffect(() => {
-    Getapi(searchValue);
-  }, [searchValue]);
+    Getapi();
+  }, []);
   return (
     <div className="App">
       <Search searchValue={searchValue} setSearchValue={setSearchValue} />
